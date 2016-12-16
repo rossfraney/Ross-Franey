3caf4a0bf5f2e225c3ec39f3a920e32680cd73f3
Victor <victorc1165@yahoo.com>
2016-12-15 21:53:43 -0800

    finished scrolling

--- js/init.js
+++ js/init.js
@@ -5,3 +5,13 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+(function($){
+	$(document).ready(function(){
+		$("a[href^='#']").click(function(){
+			$(document.body).animate({
+				'scrollTop':   $($(this).attr('href')).offset().top
+			}, 50);
+		})
+	});
+})(jQuery);
